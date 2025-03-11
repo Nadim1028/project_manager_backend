591ee17838d72db6da3a7eb217a2d1ca5d073025
Nadim1028 <bsse1028@iit.du.ac.bd>
2025-03-11 12:54:09 +0600

    initial commit

--- obj/Debug/net8.0/ProjectManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5768ac139a269754a980f58c8b3ec8baf450578")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
